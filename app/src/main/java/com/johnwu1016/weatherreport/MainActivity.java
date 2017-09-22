6348c4d178056428bb4d50d4c4c0d9b45314b9bc
wuwenjun <johnwu1016@gmail.com>
2017-09-22 09:13:49 +0800

    make an HTTP request with OkHTTP

--- app/src/main/java/com/johnwu1016/weatherreport/MainActivity.java
+++ app/src/main/java/com/johnwu1016/weatherreport/MainActivity.java
@@ -2,12 +2,38 @@ package com.johnwu1016.weatherreport;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "e17e17602b10d445c854bc3d1f9a4780";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forecastUrl = "https://api.darksky.net/forecast/" + apiKey +
+                "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(forecastUrl).build();
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            Log.v(TAG, response.body().string());
+        } catch (IOException e) {
+            Log.e(TAG, "Exception caught: ", e);
+        }
     }
 }
